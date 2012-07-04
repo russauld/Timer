24b4508275c196c253d7caba53b3718ca1cdcde3
Russell Auld <user@server>
2012-07-04 17:38:31 -0400

    Cleaned up the code and added a little information.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
+# Timer application.
+# Written by Russell Auld
 #include <QtGui/QApplication>
 #include "timer.hxx"
-//#include <time.h>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	//srand((unsigned int) time(NULL));
 	QApplication::setQuitOnLastWindowClosed(false); // without this, any dialog that gets dismissed while the main gui is hidden will kill the app
 	Timer w;
 	w.show();
